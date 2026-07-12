5d3a0a9248b92ba7fb78eb7f0e4bf8a1158555e4
Lucas Avilés <lbaviles2@gmail.com>
2026-07-12 16:18:07 -0400

    feat: agregar soporte JDBC para MySQL

--- src/test/java/com/duoc/gestionguias/GestionGuiasApplicationTests.java
+++ src/test/java/com/duoc/gestionguias/GestionGuiasApplicationTests.java
@@ -3,7 +3,9 @@ package com.duoc.gestionguias;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration"
+})
 class GestionGuiasApplicationTests {
 
 	@Test
